c4697578fd42473fff978b643a586d5a35901308
palerdot <palerdot@gmail.com>
2021-06-17 20:22:39 +0530

    progress(minigrep): case insensitive search and error handling

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -5,7 +6,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? will return error automatically
     let contents = fs::read_to_string(&config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("porumai => {}", line);
     }
 
@@ -15,6 +22,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     query: String,
     filename: String,
+    case_sensitive: bool,
 }
 
 impl Config {
@@ -26,7 +34,13 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -42,20 +56,50 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+
+    results
+}
+
 // test cases
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
     Rust:
     safe, fast, productive.
     Pick three.
+    Duct tape.
        ";
 
         assert_eq!(search(query, contents), vec!["safe, fast, productive."]);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+    Rust:
+    safe, fast, productive.
+    Pick three.
+    Trust me.
+       ";
+
+        assert_eq!(
+            search_case_insensitive(query, contents),
+            vec!["Rust:", "Trust me."]
+        );
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config: Config = Config::new(&args).unwrap_or_else(|err| {
-        println!("porumai ... parsing args - {}", err);
+        eprintln!("porumai ... parsing args - {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("porumai ... application error {}", e);
+        eprintln!("porumai ... application error {}", e);
         process::exit(1);
     }
 }
